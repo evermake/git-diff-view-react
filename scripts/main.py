11a0842534a0da3ea41fd13b4d9161232872203e
Albert Avkhadeev <albertavkhadeev@gmail.com>
2023-09-30 16:20:37 +0300

    Added line parsing along with new parsed doc

--- scripts/main.py
+++ scripts/main.py
@@ -7,24 +7,24 @@ if __name__ == "__main__":
     # Specify html doc by yourself 
     with open("my_file_diff.html", 'r', encoding="utf-8") as file:
         html_cont = file.read()
+
     soup = BeautifulSoup(html_cont, 'html.parser')
+
     divs = soup.select('div#files>div.js-diff-progressive-container>div')
-    total_lines = 0
-    anchor_line = 0
+
     my_obj = dict()
-    my_obj['totalLines'] = total_lines
+    my_obj['totalLines'] = 0
     my_obj['files'] = []
     my_obj['lines'] = []
+
     for div in divs:
         lines = []
         file = dict()
         file['path'] = div.select('a.Link--primary')[0].text
         file['isBinary'] = False
         diffs = div.select('tr[data-hunk]')
-        file['diffStart'] = anchor_line + 1
-        file['diffEnd'] = anchor_line + len(diffs)
-        anchor_line += len(diffs)
-        total_lines += len(diffs)
+        file['diffStart'] = my_obj['totalLines'] + 1
+        file['diffEnd'] = my_obj['totalLines'] + len(diffs)
         for diff in diffs:
             changes = dict()
             first_line = diff.select('td:nth-child(2)>span[data-code-marker]')
@@ -36,22 +36,31 @@ if __name__ == "__main__":
             if len(second_line) != 0:
                 second_val = second_line[0].get('data-code-marker')
             if second_val == first_val == " ":
+                my_obj['totalLines'] += 1
                 changes['type'] = "not-modified"
                 changes['content'] = diff.select('td:nth-child(4)>span')[0].text
+                changes['oldLineNo'] = diff.select('td:first-child')[0].get('data-line-number')
+                changes['newLineNo'] = diff.select('td:nth-child(3)')[0].get('data-line-number')
             elif (first_val == "-" and second_val == "+"):
+                my_obj['totalLines'] += 1   
                 changes['type'] = "modified"
                 changes['oldContent'] = diff.select('td:nth-child(2)>span')[0].text
                 changes['newContent'] = diff.select('td:nth-child(4)>span')[0].text
+                changes['oldLineNo'] = diff.select('td:first-child')[0].get('data-line-number')
+                changes['newLineNo'] = diff.select('td:nth-child(3)')[0].get('data-line-number')
             elif (first_val == " " and second_val == "+"):
+                my_obj['totalLines'] += 1
                 changes["type"] = "added"
                 changes['content'] = diff.select('td:nth-child(4)>span')[0].text
+                changes['lineNo'] = diff.select('td:nth-child(3)')[0].get('data-line-number')
             elif (first_val == "-" and second_val == " "):
+                my_obj['totalLines'] += 1
                 changes["type"] = "deleted"
                 changes['content'] = diff.select('td:nth-child(2)>span')[0].text
+                changes['lineNo'] = diff.select('td:first-child')[0].get('data-line-number')
             lines.append(changes)
         my_obj['lines'].extend(lines)
         my_obj['files'].extend([file])
-    my_obj['totalLines'] = total_lines
     # specify out document by yourself
     with open("mockdata.json", "w") as outfile:
         json.dump(my_obj, outfile)
