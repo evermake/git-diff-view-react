d3ebba5df1105b0e3eaf1959016d15ccfb0cfb3f
Albert Avkhadeev <albertavkhadeev@gmail.com>
2023-09-30 21:41:15 +0300

    Added temnaya-tema

--- src/routes/Diff.tsx
+++ src/routes/Diff.tsx
@@ -3,6 +3,7 @@ import type {
 } from '@vkontakte/vkui'
 import {
   AdaptivityProvider,
+  AppRoot,
   ConfigProvider,
   Panel,
   PanelHeader,
@@ -14,19 +15,20 @@ import {
   View,
   useAppearance,
 } from '@vkontakte/vkui'
-import { Icon28ListOutline } from '@vkontakte/icons'
+import { Icon28ListOutline, Icon28MoonOutline, Icon28SunOutline } from '@vkontakte/icons'
 import { useLocation } from 'react-router-dom'
 import queryString from 'query-string'
 import { useState } from 'react'
 import ExpandableFileBrowser from '../components/ExpandableFileBrowser'
 import type { FileDiffInfo } from '../api/api'
 import { MockDiff } from '../api/mock'
+import { useLocalStorage } from '@uidotdev/usehooks'
 
 function Diff() {
   const [showFileBrowser, setShowFileBrowser] = useState(true)
   const [files, setFiles] = useState<FileDiffInfo[]>([])
 
-  const appearance = useAppearance()
+  const [appearance, setAppearance] = useLocalStorage("theme", "dark")
   const platform: PlatformType = 'vkcom'
 
   const query = queryString.parse(useLocation().search)
@@ -41,34 +43,38 @@ function Diff() {
   return (
     <ConfigProvider
       platform={platform}
-      appearance={appearance}
+      appearance={appearance  === "dark" ? "dark" : "light"}
       isWebView={false}
+      hasCustomPanelHeaderAfter={false}
     >
       <AdaptivityProvider>
-        {commitHashesProvided
-          && <>
-              <SplitLayout>
-                <ExpandableFileBrowser show={showFileBrowser} files={files}/>
-                <SplitCol>
-                  <View activePanel='panel'>
-                    <Panel id='panel'>
-                      <PanelHeader before={<PanelHeaderButton onClick={() => setShowFileBrowser(!showFileBrowser)}> <Icon28ListOutline/> </PanelHeaderButton>} visor={true}>
-                        <Title>Comparing {hashA} and {hashB}</Title>
-                      </PanelHeader>
-                      DATA
-                    </Panel>
-                  </View>
-                </SplitCol>
-              </SplitLayout>
-          </>
-        }
+        <AppRoot>
+          {commitHashesProvided
+            && <>
+                <SplitLayout>
+                  <ExpandableFileBrowser show={showFileBrowser} files={files}/>
+                  <SplitCol>
+                    <View activePanel='panel'>
+                      <Panel id='panel'>
+                        <PanelHeader before={<PanelHeaderButton onClick={() => setShowFileBrowser(!showFileBrowser)}> <Icon28ListOutline/> </PanelHeaderButton>} visor={true} after={<PanelHeaderButton onClick={() => appearance === 'dark' ? setAppearance('light'): setAppearance('dark')}> {appearance === "dark" ? <Icon28SunOutline/> : <Icon28SunOutline/>} </PanelHeaderButton>}>
+                          
+                          <Title>Comparing {hashA} and {hashB}</Title>
+                        </PanelHeader>
+                        DATA
+                      </Panel>
+                    </View>
+                  </SplitCol>
+                </SplitLayout>
+            </>
+          }
 
-        {!commitHashesProvided
-          && <>
-            <SimpleCell>Please provide commit hashes in query like so:</SimpleCell>
-            <SimpleCell>http://localhost:5173/?a=2d0d06f&b=25a3173</SimpleCell>
-          </>
-        }
+          {!commitHashesProvided
+            && <>
+              <SimpleCell>Please provide commit hashes in query like so:</SimpleCell>
+              <SimpleCell>http://localhost:5173/?a=2d0d06f&b=25a3173</SimpleCell>
+            </>
+          }
+        </AppRoot>
       </AdaptivityProvider>
     </ConfigProvider>
   )
